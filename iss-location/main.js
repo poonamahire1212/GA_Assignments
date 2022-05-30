36110dff85c2e28c4b7b55c5020cbd6baea0f0cf
Poonam Ahire <poonamahire@192-168-1-7.tpgi.com.au>
2022-05-30 20:25:40 +1000

    changing API Url and added new icon

--- iss-location/main.js
+++ iss-location/main.js
@@ -3,8 +3,8 @@ const tileLayerUrl = "https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=K
 const attribution = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 L.tileLayer(tileLayerUrl, {attribution}).addTo(map);
 const satIcon = L.icon({
-    iconUrl: 'satellite.png',
-    iconSize: [50, 40],
+    iconUrl: 'sat.png',
+    iconSize: [60, 65],
     iconAnchor: [25, 20],
     popupAnchor: [-2, -20],
 });
@@ -12,7 +12,7 @@ const marker = L.marker([0, 0], {icon: satIcon}).addTo(map);
 marker.bindPopup("This is International Space Station").openPopup();
 
 
-const endpoint = "http://api.open-notify.org/iss-now.json";
+const endpoint = "https://api.wheretheiss.at/v1/satellites/25544";
 //const longitudePoint = document.querySelector(".longitude");
 //const latitudePoint = document.querySelector(".latitude");
 function convertToJSObject(response){
@@ -22,7 +22,7 @@ function convertToJSObject(response){
 function handleData(data){
     //longitudePoint.innerHTML = `Longitude: ${data.iss_position.longitude}`;
     //latitudePoint.innerHTML = `Latitude: ${data.iss_position.latitude}`;
-    marker.setLatLng({lat: data.iss_position.latitude, lng: data.iss_position.longitude});
+    marker.setLatLng({lat: data.latitude, lng: data.longitude});
 }
 
 function callAPI(){
